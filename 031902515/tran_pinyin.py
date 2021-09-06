76a16771b06df4cdc438461e5897b615653feab4
Jimase <90017313+Jimase@users.noreply.github.com>
2021-09-06 17:22:51 +0800

    Add files via upload

---
+++ 031902515/tran_pinyin.py
@@ -0,0 +1,11 @@
+# _*_coding :utf-8 _*_
+# @Time ;  14:35
+# @Author : @Jimase
+# @File ： tran_pinyin
+# @Project : pythonProject1
+
+import pypinyin
+
+
+from pypinyin import pinyin
+print(pinyin('中心'))
